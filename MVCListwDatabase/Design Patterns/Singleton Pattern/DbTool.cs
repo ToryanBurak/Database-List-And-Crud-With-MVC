480a213ec4193e2f4f2a6edbb4e0d6804cfc69a4
ToryanBurak <buraktoryan@gmail.com>
2023-05-20 00:39:14 +0300

    Design Patterns

---
+++ MVCListwDatabase/Design Patterns/Singleton Pattern/DbTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCListwDatabase.Design_Patterns.Singleton_Pattern
+{
+    using Models;
+    public class DbTool
+    {
+
+        DbTool() { }
+        static NorthwindEntities _dbInstance;
+        public static NorthwindEntities DbInstance()
+        {
+            if (_dbInstance == null)
+            {
+                _dbInstance = new NorthwindEntities();
+            }
+            return _dbInstance;
+        }
+    }
+}
