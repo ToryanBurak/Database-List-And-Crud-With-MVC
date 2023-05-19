0c29fbc5476377e6a84ef0a35025c92793ede711
ToryanBurak <buraktoryan@gmail.com>
2023-05-20 00:39:50 +0300

    VM

---
+++ MVCListwDatabase/ViewModels/IndexVM.cs
@@ -0,0 +1,14 @@
+﻿using MVCListwDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCListwDatabase.ViewModels
+{
+    public class IndexVM
+    {
+        public List<Employee> Employees { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
